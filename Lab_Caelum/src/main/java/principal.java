3701e0dfac9ab146d137587b00f9c756ddc7b622
Sebastiao Eudes Alves <sebastiaoeudes@bb.com.br>
2018-06-29 12:03:15 -0300

    Sexta, agora sim essa porra funciona kkkk

--- Lab_Caelum/src/main/java/principal.java
+++ Lab_Caelum/src/main/java/principal.java
@@ -4,14 +4,14 @@ public class principal {
 	public static void main(String[] args) {
         
 		Gerente novoGerente = new Gerente();
-		novoGerente.setNome("Tião Gerente");
+		novoGerente.setNome("Tiao Gerente");
 		novoGerente.setCpf("80104231149");
 		novoGerente.setSalario(4000.00);
 		
 		System.out.println("O nome do gerente : " + novoGerente.getNome());
-		System.out.println("A bonifiçãoo do gerente : "+novoGerente.getBonificacao());
+		System.out.println("A bonificacao do gerente : "+novoGerente.getBonificacao());
 		
-		System.out.println("Eu agora mando no Git");
+		System.out.println("Usando o GitHub 123");
 
 	}
 
